b75b2e827d89b611287c4216f6e12120438a3324
DevDrew01 <173702775+DevDrew01@users.noreply.github.com>
2026-04-12 20:33:53 +0800

    added route paging

--- Project/app/urls.py
+++ Project/app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import LogInPageView
+from . import views
 
 urlpatterns = [
-    path('', LogInPageView.as_view(), name='login'),
+    path('', views.home_screen, name='home'),
+    path('', views.found_view_screen, name='found_view'),
+    path('found-report/', views.found_report_screen, name='found_report'),
+    path('', views.lost_view_screen, name='lost_view'),
+    path('lost-report/', views.lost_report_screen, name='lost_report'),
+    path('', views.login_screen, name='login'),
+
 ]
--- Project/app/views.py
+++ Project/app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
-class LogInPageView(TemplateView):
-    template_name = 'app/LogInPage.html'
 
-# Create your views here.
+def home_screen(request):
+    return render(request, 'app/homepage.html')
+
+def lost_view_screen(request):
+    return render(request, 'app/lostviewpage.html')
+
+def lost_report_screen(request):
+    return render(request, 'app/lostreportpage.html')
+
+def found_view_screen(request):
+    return render(request, 'app/foundviewpage.html')
+
+def found_report_screen(request):
+    return render(request, 'app/foundreportpage.html')
+
+def login_screen(request):
+    return render(request, 'app/loginpage.html')
