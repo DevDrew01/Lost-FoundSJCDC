7653de7bf25e15e27469bafa4e0e5f9b81ba1dde
DevDrew01 <173702775+DevDrew01@users.noreply.github.com>
2026-04-16 11:24:55 +0800

    added postgres driver

--- Project/settings.py
+++ Project/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'nandito-ako.up.railway.app']
 
 
 # Application definition
