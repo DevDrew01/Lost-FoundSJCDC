f3d75a58bea30d58fda621a684f2e01824c2cd5c
DevDrew01 <173702775+DevDrew01@users.noreply.github.com>
2026-04-16 11:30:46 +0800

    fix

--- Project/settings.py
+++ Project/settings.py
@@ -113,8 +113,9 @@ CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=env(default='sqlite:///db.sqlite3'),
-        conn_max_age=600
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        engine='django.db.backends.postgresql' # Forces the postgres engine
     )
 }
 
